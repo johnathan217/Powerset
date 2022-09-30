11292fb919f9564b5f476e30ba9f990c3af84e29
johnathan217 <113518999+johnathan217@users.noreply.github.com>
2022-09-30 11:30:01 +0100

    Add files via upload

--- powerset_function.py
+++ powerset_function.py
@@ -1,20 +1,19 @@
-
 # powerset generates binary of 1 to 2^input length (or, number of all possible unique subsets) and checks each bit
 # sequentially. 1 results in the data indexed in the position of the bit being checked relative to the total bits
 # present being added to the subset, and 0 results in nothing. Each subset is added to a list.
 def powerset(x):
-    powerlist = []
+    powerset.powerlist = []
     temp = []
     for i in range(2 ** len(x)):
         s = list((bin(i).replace("0b", "")))
         if i > 0:
-            powerlist.append(temp)
+            powerset.powerlist.append(temp)
         temp = []
         for p in range(len(s)):
             if s[p] == '1':
                 temp.append(x[len(s) - p - 1])
-    powerlist.append(temp)
-    print(powerlist)
+    powerset.powerlist.append(temp)
 
 
-powerset([1, 2, 3])
+powerset([1, 2, 3, 4])
+print(powerset.powerlist)
